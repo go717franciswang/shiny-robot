4997e547cc259392247eedde21dc32d43b48db8b
francis <go717franciswang@gmail.com>
2014-04-04 19:05:32 -0700

    Add builder shell

---
+++ query_builder.py
@@ -0,0 +1,17 @@
+class QueryBuilder:
+    def add_table(self, t):
+        """docstring for add_table"""
+        pass
+
+    def alias(self, a):
+        """docstring for alias"""
+        pass
+
+    def link_tables(self, tx, ty):
+        """docstring for link_tables"""
+        pass
+
+    def select(self, aliases):
+        """docstring for select"""
+        pass
+
