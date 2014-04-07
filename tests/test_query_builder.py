e4479cab2f117157dd3c2eadbee6824c19a08fe0
Francis Wang <go717franciswang@hotmail.com>
2014-04-06 23:49:29 -0700

    Correct test

--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -41,74 +41,76 @@ class TestQueryBuilder(unittest.TestCase):
         query = self.builder.select(['fa', 'fb'])
         self.assertQuery(query, '''
             select a.f fa, b.f fb 
-            from table_a a 
-            join table_b b 
+            from table_a a,
+                table_b b 
             where a.id = b.id''')
 
     def testSelectAC(self):
         query = self.builder.select(['fa', 'fc'])
         self.assertQuery(query, '''
             select a.f fa, c.f fc 
-            from table_a a 
-            join table_c c 
+            from table_a a,
+                table_c c 
             where a.id = c.id''')
 
     def testSelectAD(self):
         query = self.builder.select(['fa', 'fd'])
         self.assertQuery(query, '''
             select a.f fa, d.f fd 
-            from table_a a 
-            join table_c c  
-            join table_d d 
+            from table_a a,
+                table_c c, 
+                table_d d 
             where a.id = c.id and c.id = d.id''')
 
     def testSelectBC(self):
         query = self.builder.select(['fb', 'fc'])
         self.assertQuery(query, '''
             select b.f fb, c.f fc 
-            from table_a a 
-            join table_b b 
-            join table_c c 
+            from table_a a,
+                table_b b,
+                table_c c 
             where a.id = b.id and a.id = c.id''')
 
     def testSelectBD(self):
         query = self.builder.select(['fb', 'fd'])
         self.assertQuery(query, '''
             select b.f fb, d.f fd 
-            from table_a a 
-            join table_b b 
-            join table_c c 
-            join table_d d 
+            from table_a a,
+                table_b b, 
+                table_c c, 
+                table_d d 
             where a.id = b.id and a.id = c.id and c.id = d.id''')
 
     def testSelectCD(self):
         query = self.builder.select(['fc', 'fd'])
         self.assertQuery(query, '''
             select c.f fc, d.f fd 
-            from table_c c 
-            join table_d d 
+            from table_c c,
+                table_d d 
             where c.id = d.id''')
 
     def testSelectABC(self):
         query = self.builder.select(['fa', 'fb', 'fc'])
         self.assertQuery(query, '''
             select a.f fa, b.f fb, c.f fc 
-            from table_a a 
-            join table_b b 
-            join table_c c 
+            from table_a a,
+                table_b b, 
+                table_c c 
             where a.id = b.id and a.id = c.id''')
 
     def testSelectABCD(self):
         query = self.builder.select(['fa', 'fb', 'fc', 'fd'])
         self.assertQuery(query, '''
             select a.f fa, b.f fb, c.f fc, d.f fd 
-            from table_a a 
-            join table_b b 
-            join table_c c 
-            join table_d d 
+            from table_a a, 
+                table_b b, 
+                table_c c, 
+                table_d d
             where a.id = b.id and a.id = c.id and c.id = d.id''')
 
     def assertQuery(self, q1, q2):
         q1 = re.sub('\s+', ' ', q1)
-        q2 = re.sub('\s+', ' ', q1)
+        q2 = re.sub('\s+', ' ', q2)
+        # print "q1: \n" + q1
+        # print "q2: \n" + q2
         self.assertEquals(q1, q2)
