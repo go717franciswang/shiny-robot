a1fad54e925026f538260546831840d369a413cb
Francis Wang <francis@bposolutions.com>
2014-04-08 15:59:04 -0700

    Threading problem

--- query_result_aggregator.py
+++ query_result_aggregator.py
@@ -1,11 +1,15 @@
 from query_builder import QueryBuilder
-from queue import Queue
+from Queue import Queue
+import sqlite3
+from threading import Thread
 
 class QueryResultAggregator:
     def __init__(self):
         self._max_concurrent_queries = 1
-        self._schema = []
+        self._schema = None
         self._unprocessed_queries = Queue()
+        self._conn = sqlite3.connect(':memory:')
+        self._insertion_query = None
 
     def set_max_concurrent_queries(self, max_concurrent_queries):
         if max_concurrent_queries < 1:
@@ -14,10 +18,60 @@ class QueryResultAggregator:
 
     def set_schema(self, schema):
         self._schema = schema
+        self._create_tmp()
+        self._setup_insertion_query()
 
-    def add_query(self, query, cursor):
-        self._unprocessed_queries.put((query, cursor))
+    def add_query(self, query, connection):
+        self._unprocessed_queries.put((query, connection))
 
-    def aggregate(self):
-        """docstring for aggregate"""
-        pass
+    def _create_tmp(self):
+        q = 'create table if not exists tmp ('
+        q += ', '.join(self._schema)
+        q += ')'
+        c = self._conn.cursor()
+        c.execute(q)
+        c.close()
+
+    def _setup_insertion_query(self):
+        self._insertion_query = 'insert into tmp '
+        self._insertion_query += 'values (?' + ',?' * (len(self._schema)-1) + ')'
+
+    def _query(self):
+        while True:
+            q, conn = self._unprocessed_queries.get()
+            c = conn.cursor()
+            rs = c.execute(q)
+
+            # start the next query as soon as this one completes
+            self._start_query()
+
+            for row in rs:
+                c.execute(q, self._norm_row(row))
+            c.close()
+            self._unprocessed_queries.task_done()
+
+    def _norm_row(self, row):
+        res = []
+        for i in range(len(row)):
+            typeof = type(row[i])
+            if typeof == decimal.Decimal:
+                res.append(float(row[i]))
+            elif typeof == bytes or typeof == bytearray:
+                res.append(row[i].decode())
+            elif typeof == datetime.date:
+                res.append(row[i].strftime('%Y-%m-%d'))
+            else: res.append(row[i])
+        return res
+
+    def _start_query(self):
+        t = Thread(target=self._query)
+        t.daemon = True
+        t.start()
+
+    def aggregate(self, query):
+        for i in range(self._max_concurrent_queries):
+            self._start_query()
+
+        self._unprocessed_queries.join()
+        c = self._conn.cursor()
+        return c.execute(query)
--- tests/test_query_result_aggregator.py
+++ tests/test_query_result_aggregator.py
@@ -1,2 +1,27 @@
 import unittest
+import sqlite3
+from query_result_aggregator import QueryResultAggregator
+
+class TestQueryResultAggregator(unittest.TestCase):
+    def setUp(self):
+        self.conn = sqlite3.connect('tests/Chinook_Sqlite_AutoIncrementPKs.sqlite')
+        self.a = QueryResultAggregator()
+        self.a.set_schema(['q integer'])
+
+    def testAggregate(self):
+        for i in range(1, 11):
+            self.a.add_query(
+                    'select sum(quantity) q from invoiceline where invoiceid = %d' % (i,), 
+                    self.conn)
+        q = self.a.aggregate('select sum(q) from tmp').fetchone()[0]
+
+        c = self.conn.cursor()
+        c.execute('select sum(quantity) from invoiceline where invoiceid <= 10')
+        q2 = c.fetchone()[0]
+
+        self.assertEquals(q, q2)
+
+
+
+
 
