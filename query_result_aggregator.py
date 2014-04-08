9db0324ce7824cac2611c614991442270ee0910e
Francis Wang <francis@bposolutions.com>
2014-04-07 18:00:25 -0700

    Implement add_query

--- query_result_aggregator.py
+++ query_result_aggregator.py
@@ -1,13 +1,22 @@
 from query_builder import QueryBuilder
+from queue import Queue
 
 class QueryResultAggregator:
     def __init__(self):
-        """docstring for __init__"""
-        pass
+        self._max_concurrent_queries = 1
+        self._schema = []
+        self._unprocessed_queries = Queue()
+
+    def set_max_concurrent_queries(self, max_concurrent_queries):
+        if max_concurrent_queries < 1:
+            raise ValueError('max concurrent queries must be > 0')
+        self._max_concurrent_queries = max_concurrent_queries
+
+    def set_schema(self, schema):
+        self._schema = schema
 
     def add_query(self, query, cursor):
-        """docstring for add_query"""
-        pass
+        self._unprocessed_queries.put((query, cursor))
 
     def aggregate(self):
         """docstring for aggregate"""
