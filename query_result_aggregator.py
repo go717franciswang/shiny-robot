57a069356a1d93ae74695707d6e4973fa930567a
Francis Wang <francis@bposolutions.com>
2014-04-07 17:50:26 -0700

    Add query_result_aggregator shell

---
+++ query_result_aggregator.py
@@ -0,0 +1,14 @@
+from query_builder import QueryBuilder
+
+class QueryResultAggregator:
+    def __init__(self):
+        """docstring for __init__"""
+        pass
+
+    def add_query(self, query, cursor):
+        """docstring for add_query"""
+        pass
+
+    def aggregate(self):
+        """docstring for aggregate"""
+        pass
