cd1d37c248199997b11df42e64742e165f0d2d8d
Francis Wang <francis@bposolutions.com>
2014-04-11 18:14:26 -0700

    Test autoload mysql table

--- tests/test_query_builder_add_table_with_fields.py
+++ tests/test_query_builder_add_table_with_fields.py
@@ -1,11 +1,11 @@
 from tests.test_query_builder_base import TestQueryBuilderBase
 from query_builder import QueryBuilder
-import configparser
+import ConfigParser
 
 class TestQueryBuilderAddTableWithFields(TestQueryBuilderBase):
     def setUp(self):
-        config = configparser.RawConfigParser()
-        config.read('tests/db_cred.ini')
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read('tests/db_cred.ini')
 
     def testSqlite3(self):
         import sqlite3
@@ -18,4 +18,12 @@ class TestQueryBuilderAddTableWithFields(TestQueryBuilderBase):
     def testMySql(self):
         import mysql.connector as mdb
         builder = QueryBuilder()
-        conn = sqlite
+        conn = mdb.connect(
+                host=self.config.get('mysql', 'host'), 
+                port=self.config.get('mysql', 'port'),
+                user=self.config.get('mysql', 'user'),
+                passwd=self.config.get('mysql', 'passwd'))
+        builder.add_table_with_fields('test.album', 'a', conn)
+        query = builder.select(['albumid', 'artistid'])
+        self.assertQuery(query, 'select a.albumid albumid, a.artistid artistid from test.album a')
+
